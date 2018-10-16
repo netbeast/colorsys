b71c549f34c640d538d4b701f637f74c149cadba
Jesús Darío <jsdario@users.noreply.github.com>
2018-10-16 17:58:37 +0200

    Revert "decimal2Hex and hex2Decimal and test (#20)"
    This reverts commit ff3a7d06066dbc53b7e7993c34387adbcd825a4a.

--- colorsys.js
+++ colorsys.js
@@ -1,26 +1,23 @@
+
 const RGB_MAX = 255
 const HUE_MAX = 360
 const SV_MAX = 100
 
-var colorsys = (module.exports = {})
+var colorsys = module.exports = {}
 
-colorsys.rgb2Hsl = function(r, g, b) {
-  if (typeof r === "object") {
+colorsys.rgb2Hsl = function (r, g, b) {
+  if (typeof r === 'object') {
     const args = r
-    r = args.r
-    g = args.g
-    b = args.b
+    r = args.r; g = args.g; b = args.b;
   }
   // It converts [0,255] format, to [0,1]
-  r = r === RGB_MAX ? 1 : (r % RGB_MAX) / parseFloat(RGB_MAX)
-  g = g === RGB_MAX ? 1 : (g % RGB_MAX) / parseFloat(RGB_MAX)
-  b = b === RGB_MAX ? 1 : (b % RGB_MAX) / parseFloat(RGB_MAX)
+  r = (r === RGB_MAX) ? 1 : (r % RGB_MAX / parseFloat(RGB_MAX))
+  g = (g === RGB_MAX) ? 1 : (g % RGB_MAX / parseFloat(RGB_MAX))
+  b = (b === RGB_MAX) ? 1 : (b % RGB_MAX / parseFloat(RGB_MAX))
 
   var max = Math.max(r, g, b)
   var min = Math.min(r, g, b)
-  var h,
-    s,
-    l = (max + min) / 2
+  var h, s, l = (max + min) / 2
 
   if (max === min) {
     h = s = 0 // achromatic
@@ -50,24 +47,20 @@ colorsys.rgb2Hsl = function(r, g, b) {
 
 colorsys.rgb_to_hsl = colorsys.rgbToHsl = colorsys.rgb2Hsl
 
-colorsys.rgb2Hsv = function(r, g, b) {
-  if (typeof r === "object") {
+colorsys.rgb2Hsv = function (r, g, b) {
+  if (typeof r === 'object') {
     const args = r
-    r = args.r
-    g = args.g
-    b = args.b
+    r = args.r; g = args.g; b = args.b;
   }
 
   // It converts [0,255] format, to [0,1]
-  r = r === RGB_MAX ? 1 : (r % RGB_MAX) / parseFloat(RGB_MAX)
-  g = g === RGB_MAX ? 1 : (g % RGB_MAX) / parseFloat(RGB_MAX)
-  b = b === RGB_MAX ? 1 : (b % RGB_MAX) / parseFloat(RGB_MAX)
+  r = (r === RGB_MAX) ? 1 : (r % RGB_MAX / parseFloat(RGB_MAX))
+  g = (g === RGB_MAX) ? 1 : (g % RGB_MAX / parseFloat(RGB_MAX))
+  b = (b === RGB_MAX) ? 1 : (b % RGB_MAX / parseFloat(RGB_MAX))
 
   var max = Math.max(r, g, b)
   var min = Math.min(r, g, b)
-  var h,
-    s,
-    v = max
+  var h, s, v = max
 
   var d = max - min
 
@@ -99,26 +92,24 @@ colorsys.rgb2Hsv = function(r, g, b) {
 
 colorsys.rgb_to_hsv = colorsys.rgbToHsv = colorsys.rgb2Hsv
 
-colorsys.hsl2Rgb = function(h, s, l) {
-  if (typeof h === "object") {
+colorsys.hsl2Rgb = function (h, s, l) {
+  if (typeof h === 'object') {
     const args = h
-    h = args.h
-    s = args.s
-    l = args.l
+    h = args.h; s = args.s; l = args.l;
   }
 
   var r, g, b
 
   h = _normalizeAngle(h)
-  h = h === HUE_MAX ? 1 : (h % HUE_MAX) / parseFloat(HUE_MAX)
-  s = s === SV_MAX ? 1 : (s % SV_MAX) / parseFloat(SV_MAX)
-  l = l === SV_MAX ? 1 : (l % SV_MAX) / parseFloat(SV_MAX)
+  h = (h === HUE_MAX) ? 1 : (h % HUE_MAX / parseFloat(HUE_MAX))
+  s = (s === SV_MAX) ? 1 : (s % SV_MAX / parseFloat(SV_MAX))
+  l = (l === SV_MAX) ? 1 : (l % SV_MAX / parseFloat(SV_MAX))
 
   if (s === 0) {
     r = g = b = l // achromatic
   } else {
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s
-    var p = 2 * l - q
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
     r = _hue2Rgb(p, q, h + 1 / 3)
     g = _hue2Rgb(p, q, h)
     b = _hue2Rgb(p, q, h - 1 / 3)
@@ -127,24 +118,22 @@ colorsys.hsl2Rgb = function(h, s, l) {
   return {
     r: Math.round(r * RGB_MAX),
     g: Math.round(g * RGB_MAX),
-    b: Math.round(b * RGB_MAX)
+    b: Math.round(b * RGB_MAX),
   }
 }
 
 colorsys.hsl_to_rgb = colorsys.hslToRgb = colorsys.hsl2Rgb
 
-colorsys.hsv2Rgb = function(h, s, v) {
-  if (typeof h === "object") {
+colorsys.hsv2Rgb = function (h, s, v) {
+  if (typeof h === 'object') {
     const args = h
-    h = args.h
-    s = args.s
-    v = args.v
+    h = args.h; s = args.s; v = args.v;
   }
 
   h = _normalizeAngle(h)
-  h = h === HUE_MAX ? 1 : ((h % HUE_MAX) / parseFloat(HUE_MAX)) * 6
-  s = s === SV_MAX ? 1 : (s % SV_MAX) / parseFloat(SV_MAX)
-  v = v === SV_MAX ? 1 : (v % SV_MAX) / parseFloat(SV_MAX)
+  h = (h === HUE_MAX) ? 1 : (h % HUE_MAX / parseFloat(HUE_MAX) * 6)
+  s = (s === SV_MAX) ? 1 : (s % SV_MAX / parseFloat(SV_MAX))
+  v = (v === SV_MAX) ? 1 : (v % SV_MAX / parseFloat(SV_MAX))
 
   var i = Math.floor(h)
   var f = h - i
@@ -159,47 +148,43 @@ colorsys.hsv2Rgb = function(h, s, v) {
   return {
     r: Math.floor(r * RGB_MAX),
     g: Math.floor(g * RGB_MAX),
-    b: Math.floor(b * RGB_MAX)
+    b: Math.floor(b * RGB_MAX),
   }
 }
 
 colorsys.hsv_to_rgb = colorsys.hsv2Rgb
 colorsys.hsvToRgb = colorsys.hsv2Rgb
 
-colorsys.rgb2Hex = function(r, g, b) {
-  if (typeof r === "object") {
+colorsys.rgb2Hex = function (r, g, b) {
+  if (typeof r === 'object') {
     const args = r
-    r = args.r
-    g = args.g
-    b = args.b
+    r = args.r; g = args.g; b = args.b;
   }
   r = Math.round(r).toString(16)
   g = Math.round(g).toString(16)
   b = Math.round(b).toString(16)
 
-  r = r.length === 1 ? "0" + r : r
-  g = g.length === 1 ? "0" + g : g
-  b = b.length === 1 ? "0" + b : b
+  r = r.length === 1 ? '0' + r : r
+  g = g.length === 1 ? '0' + g : g
+  b = b.length === 1 ? '0' + b : b
 
-  return "#" + r + g + b
+  return '#' + r + g + b
 }
 
 colorsys.rgb_to_hex = colorsys.rgbToHex = colorsys.rgb2Hex
 
-colorsys.hex2Rgb = function(hex) {
+colorsys.hex2Rgb = function (hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      }
-    : null
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null
 }
 
 colorsys.hex_to_rgb = colorsys.hexToRgb = colorsys.hex2Rgb
 
-colorsys.hsv2Hex = function(h, s, v) {
+colorsys.hsv2Hex = function (h, s, v) {
   var rgb = colorsys.hsv2Rgb(h, s, v)
   return colorsys.rgb2Hex(rgb.r, rgb.g, rgb.b)
 }
@@ -207,33 +192,31 @@ colorsys.hsv2Hex = function(h, s, v) {
 colorsys.hsv_to_hex = colorsys.hsv2Hex
 colorsys.hsvToHex = colorsys.hsv2Hex
 
-colorsys.hex2Hsv = function(hex) {
+colorsys.hex2Hsv = function (hex) {
   var rgb = colorsys.hex2Rgb(hex)
   return colorsys.rgb2Hsv(rgb.r, rgb.g, rgb.b)
 }
 
 colorsys.hex_to_hsv = colorsys.hexToHsv = colorsys.hex2Hsv
 
-colorsys.hsl2Hex = function(h, s, l) {
+colorsys.hsl2Hex = function (h, s, l) {
   var rgb = colorsys.hsl2Rgb(h, s, l)
   return colorsys.rgb2Hex(rgb.r, rgb.g, rgb.b)
 }
 
 colorsys.hsl_to_hex = colorsys.hslToHex = colorsys.hsl2Hex
 
-colorsys.hex2Hsl = function(hex) {
+colorsys.hex2Hsl = function (hex) {
   var rgb = colorsys.hex2Rgb(hex)
   return colorsys.rgb2Hsl(rgb.r, rgb.g, rgb.b)
 }
 
 colorsys.hex_to_hsl = colorsys.hexToHsl = colorsys.hex2Hsl
 
-colorsys.rgb2Cmyk = function(r, g, b) {
-  if (typeof r === "object") {
+colorsys.rgb2Cmyk = function (r, g, b) {
+  if (typeof r === 'object') {
     const args = r
-    r = args.r
-    g = args.g
-    b = args.b
+    r = args.r; g = args.g; b = args.b;
   }
 
   var rprim = r / 255
@@ -256,13 +239,10 @@ colorsys.rgb2Cmyk = function(r, g, b) {
 
 colorsys.rgb_to_cmyk = colorsys.rgbToCmyk = colorsys.rgb2Cmyk
 
-colorsys.cmyk2Rgb = function(c, m, y, k) {
-  if (typeof c === "object") {
+colorsys.cmyk2Rgb = function (c, m, y, k) {
+  if (typeof c === 'object') {
     const args = c
-    c = args.c
-    m = args.m
-    y = args.y
-    k = args.k
+    c = args.c; m = args.m; y = args.y; k = args.k;
   }
 
   var r = 255 * (1 - c) * (1 - k)
@@ -278,23 +258,21 @@ colorsys.cmyk2Rgb = function(c, m, y, k) {
 
 colorsys.cmyk_to_rgb = colorsys.cmykToRgb = colorsys.cmyk2Rgb
 
-colorsys.hsv2Hsl = function(h, s, v) {
-  if (typeof h === "object") {
+colorsys.hsv2Hsl = function (h, s, v) {
+  if (typeof h === 'object') {
     const args = h
-    h = args.h
-    s = args.s
-    v = args.v
+    h = args.h; s = args.s; v = args.v;
   }
 
-  var l = ((2 - s) * v) / 2
+  var l = (2 - s) * v / 2
 
   if (l !== 0) {
     if (l === SV_MAX) {
       s = 0
     } else if (l < SV_MAX / 2) {
-      s = (s * v) / (l * 2)
+      s = s * v / (l * 2)
     } else {
-      s = (s * v) / (2 - l * 2)
+      s = s * v / (2 - l * 2)
     }
   }
 
@@ -303,42 +281,37 @@ colorsys.hsv2Hsl = function(h, s, v) {
 
 colorsys.hsv_to_hsl = colorsys.hsvToHsl = colorsys.hsv2Hsl
 
-colorsys.hsl2Hsv = function(h, s, l) {
-  if (typeof h === "object") {
+colorsys.hsl2Hsv = function (h, s, l) {
+  if (typeof h === 'object') {
     const args = h
-    h = args.h
-    s = args.s
-    l = args.l
+    h = args.h; s = args.s; l = args.l;
   }
 
-  s = s * (l < 50 ? l : 100 - l)
+  s = s * (l < 50 ? l : (100 - l))
 
   return {
     h: h,
-    s: Math.floor((2 * s) / (l + s)),
-    v: Math.floor(l + s)
+    s: Math.floor(2 * s / (l + s)),
+    v: Math.floor(l + s),
   }
 }
 
 colorsys.hsl_to_hsv = colorsys.hslToHsv = colorsys.hsl2Hsv
 
 /**
- * Parses values from a string into a javascript object
- * e.g: hsla(140, 30%, 40%, .5) => { h: 140, s: 30, l: 40, alpha: 0.5}
- */
-colorsys.parseCss = function(cssString) {
-  if (cssString.indexOf("#") > -1) {
+* Parses values from a string into a javascript object
+* e.g: hsla(140, 30%, 40%, .5) => { h: 140, s: 30, l: 40, alpha: 0.5}
+*/
+colorsys.parseCss = function (cssString) {
+  if (cssString.indexOf('#') > -1) {
     return colorsys.hex2Rgb(cssString)
   }
 
-  const prefix = cssString.split("(")[0]
-  const args = cssString
-    .split("(")[1]
-    .split(")")[0]
-    .split(",")
+  const prefix = cssString.split('(')[0]
+  const args = cssString.split('(')[1].split(')')[0].split(',')
 
   // Use the prefix as an array [r, g, b, a] to parse the colours
-  return prefix.split("").reduce(function(color, param, idx) {
+  return prefix.split('').reduce(function (color, param, idx) {
     const nextColor = color
     nextColor[param] = parseFloat(args[idx])
     return nextColor
@@ -347,66 +320,48 @@ colorsys.parseCss = function(cssString) {
 
 colorsys.parse_css = colorsys.parseCss
 
-colorsys.stringify = function(obj) {
-  const prefix = Object.keys(obj).join("")
-  const values = Object.keys(obj).map(function(key) {
+colorsys.stringify = function (obj) {
+  const prefix = Object.keys(obj).join('')
+  const values = Object.keys(obj).map(function (key) {
     var val = obj[key]
-    if (key === "s" || key === "v" || key === "l") {
-      val = val + "%"
+    if (key === 's' || key === 'v' || key === 'l') {
+      val = val + '%'
     }
     return val
   })
-  return prefix + "(" + values.join(", ") + ")"
-}
-
-// Google Assistant API uses this format in SmartHome Apps. Example => "spectrumRGB": 16711680
-colorsys.hex_to_decimal = colorsys.hexToDecimal = colorsys.hex2Decimal
-
-colorsys.hex2Decimal = function(hexColor) {
-  if (typeof hexColor === "string") {
-    return parseInt(hexColor.replace("#", ""), 16)
-  }
-}
-
-colorsys.decimal_to_hex = colorsys.decimalToHex = colorsys.decimal2Hex
-
-colorsys.decimal2Hex = function(decimalColor) {
-  if (typeof decimalColor === "string") {
-    return "#" + parseInt(decimalColor).toString(16)
-  }
-  return "#" + decimalColor.toString(16)
+  return prefix + '(' + values.join(', ') + ')'
 }
 
 // Will return a random hex colour
-colorsys.random = function() {
-  const base = "000000"
+colorsys.random = function () {
+  const base = '000000'
   const number = Math.floor(Math.random() * 16777215).toString(16)
-  return "#" + (base + number).substr(-6)
+  return '#' + (base + number).substr(-6)
 }
 
-colorsys.rotateHue = function(hue, amount) {
-  if (amount === void 0) {
-    amount = 0
-  }
-  const aux =
-    typeof hue === "object" ? (hue.h + amount) % 360 : (hue + amount) % 360
-
-  const nextHue = aux < 0 ? 360 + aux : aux
-  return typeof hue === "object" ? Object.assign(hue, { h: nextHue }) : nextHue
+colorsys.rotateHue = function (hue, amount) {
+  if (amount === void 0) { amount = 0; }
+  const aux = typeof hue === 'object'
+      ? (hue.h + amount) % 360
+      : (hue + amount) % 360
+      
+  const nextHue = aux < 0 ? (360 + aux) : aux
+  return typeof hue === 'object'
+      ? Object.assign(hue, { h: nextHue })
+      : nextHue
 }
 
-colorsys.getColorEncoding = function(color) {
-  if (typeof color === "string") {
+
+colorsys.getColorEncoding = function (color) {
+  if (typeof color === 'string') {
     try {
       colorsys.hex2Rgb(color)
-      return "hex"
-    } catch (err) {
-      /* Silent catch */
-    }
+      return 'hex'
+    } catch (err) { /* Silent catch */ }
   }
 
-  if (typeof color !== "object") {
-    return "unknown"
+  if (typeof color !== 'object') {
+    return 'unknown'
   }
 
   // Now check that the sum of the components is still a number
@@ -429,14 +384,14 @@ colorsys.getColorEncoding = function(color) {
     return 'cmyk'
   }
 
-  return "unknown"
+  return 'unknown'
 }
 
-function _normalizeAngle(degrees) {
-  return ((degrees % 360) + 360) % 360
+function _normalizeAngle (degrees) {
+  return (degrees % 360 + 360) % 360;
 }
 
-function _hue2Rgb(p, q, t) {
+function _hue2Rgb (p, q, t) {
   if (t < 0) t += 1
   if (t > 1) t -= 1
   if (t < 1 / 6) return p + (q - p) * 6 * t
